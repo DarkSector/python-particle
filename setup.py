cbf0054126c22d4706749b4010d433d3e8bf2774
Pronoy Chopra <pronoyc@gmail.com>
2015-08-21 01:28:40 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='python-particle',
+    version='0.1',
+    packages=['particle'],
+    url='',
+    license='MIT',
+    author='DarkSector',
+    description='A Python wrapper around the Particle (particle.io) Cloud API'
+)
